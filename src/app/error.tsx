9c6a54fbb0db26d4046546756f43b524c38d9314
JiinSeok <126224323+JiinSeok@users.noreply.github.com>
2025-02-23 17:59:05 +0900

    feat: add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function ErrorPage() {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4">
+      <h1 className="text-4xl font-bold text-center"></h1>
+      <p className="text-lg text-center">please refresh the page.</p>
+    </div>
+  )
+}
