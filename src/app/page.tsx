f5f193815a8b1a7f330e3de018ad18916d056555
JiinSeok <126224323+jiinseok@users.noreply.github.com>
2025-02-25 18:56:42 +0900

    refactor: rename Chart, Search section components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import ChartSection from '@/app/[locale]/trade/components/ChartSection'
+import CoinChart from '@/app/[locale]/trade/components/CoinChart'
+import CoinSearch from '@/app/[locale]/trade/components/CoinSearch'
 import MarketTrades from '@/app/[locale]/trade/components/MarketTrades'
 import OpenOrders from '@/app/[locale]/trade/components/OpenOrders'
 import OrderBook from '@/app/[locale]/trade/components/OrderBook'
-import PriceSearch from '@/app/[locale]/trade/components/PriceSearch'
 import TopMovers from '@/app/[locale]/trade/components/TopMovers'
 import TradeConsole from '@/app/[locale]/trade/components/TradeConsole'
 import TradeOverview from '@/app/[locale]/trade/components/TradeOverview'
@@ -11,11 +11,11 @@ export default function Home() {
   return (
     <>
       <main>
-        <div className="flex-auto grid gap-1 p-4 grid-cols-12 grid-rows-19">
-          <TradeOverview />
+        <div className="relative flex-auto max-w-7xl max-h-fit grid gap-1 p-4 grid-cols-12 grid-rows-19">
+          <TradeOverview symbol={'BTCUSDT'} />
           <OrderBook />
-          <ChartSection />
-          <PriceSearch />
+          <CoinChart />
+          <CoinSearch />
           <TradeConsole />
           <MarketTrades />
           <TopMovers />
